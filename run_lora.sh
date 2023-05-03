f17bb2ca391d54639187711413b63dad844c9f3a
Hideto Obara <hidetobara@gmail.com>
2023-05-03 17:39:44 +0900

    ファイルを整理

--- run_lora.sh
+++ run_lora.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 accelerate launch --num_cpu_threads_per_process 1 train_network.py \
-    --pretrained_model_name_or_path="/data/StableDiffusion/orangechillmix_v70.safetensors" \
-    --dataset_config="lora.toml" \
+    --pretrained_model_name_or_path="/data/StableDiffusion/coremixpure_v10.safetensors" \
+    --dataset_config="lora_sch.toml" \
     --output_dir="/app/out/" \
-    --output_name="lala" \
+    --output_name="sch" \
     --save_model_as=safetensors \
     --prior_loss_weight=1.0 \
-    --max_train_steps=400 \
+    --max_train_steps=1500 \
     --learning_rate=1e-4 \
     --optimizer_type="AdamW8bit" \
     --mixed_precision="no" \
     --cache_latents \
     --gradient_checkpointing \
-    --save_every_n_epochs=1 \
+    --save_every_n_epochs=3 \
     --xformers \
     --network_module=networks.lora
